74d3a030cb537412650b054d80fa5aa46144a03a
Diego Perazzolo <diegoperazzolo8@gmail.com>
2023-06-16 11:02:45 +0200

    16-06-23 Update method_1_DCT.py
    Code used to generate augmented images of dataset "data_augmeted_m1"

--- method_1_DCT.py
+++ method_1_DCT.py
@@ -14,15 +14,24 @@ def method1_DCT(im):
     image = np.zeros_like(im, dtype=np.uint8)
     for i in range(3):
         # apply DCT to every dimension of the image
-        DCT = dct(dct(im[:, :, i], axis=0), axis=1)
-        d1 = DCT.copy() # a copy for unmodified pixel in opsition 1. 
+        DCT = dct(dct(im[:, :, i], axis=0, norm='ortho'), axis=1, norm='ortho')
+        d1 = DCT.copy() # a copy for unmodified pixel in position 1. 
         # set some pixel to 0
         # changing the probability vector you can change the selection
-        zero_indices = np.random.choice([False, True], size=DCT.shape, p=[0.6, 0.4]) # 0.6 probability of TRUE and 0.4 probability of FALSE. 
+        zero_indices = np.random.choice([False, True], size=DCT.shape, p=[0.4, 0.6]) # 0.6 probability of TRUE and 0.4 probability of FALSE. 
+        #----------------------------------------------------------------------------
+        # set all the pixels that have a value higher than (mode + std*2) or lower than (mode - std*2) to a value of zero
+        values, counts = np.unique(im, return_counts=True)
+        mode_value = values[np.argmax(counts)]
+        std_dev = np.std(im)
+        condition1 = np.abs(DCT) < (mode_value - std_dev*2)
+        condition2 = np.abs(DCT) > (mode_value + std_dev*2)
+        zero_indices = np.logical_or(condition1, condition2) # all pixels lower than threshold are setted to zero. 
         DCT[zero_indices] = 0
+        #----------------------------------------------------------------------------        
         # leave unmodified pixel in position (1,1)
         DCT[0, 0] = d1[0, 0]
         # apply inverse DCT
-        image[:, :, i] = idct(idct(DCT, axis=0), axis=1)
+        image[:, :, i] = idct(idct(DCT, axis=0, norm='ortho'), axis=1, norm='ortho')
     # return the image
     return image.astype(np.uint8)
